deb243ccd6d7b5fd6cb243284bf4328117cfea81
unknown <samuelrmajor@gmail.com>
2022-02-11 17:55:06 -0500

    Exercise 1.13+1.14

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -10,14 +10,46 @@ const Button = ({ handleClick, text }) => (
   </button>
 )
 
+const Popanec = ({popularAnecdote,anecdotes,votes}) => {
+  if (popularAnecdote == -1)
+    return (
+      <div>
+      <h1>
+        Current Popular Header:
+      </h1>
+        No Votes have been cast
+      </div>
 
+    )
+  return (
+      <div>
+      <h1>
+        Current Popular Header:
+      </h1>
+        {anecdotes[popularAnecdote]}
+        <br/>
+        {votes[popularAnecdote]} Votes
+      </div>
+
+    )
+  }
 
 
 
 const App = () => {
 
+  
+
   const generateAnecdote = () =>{
     setSelected(Math.floor(Math.random() * anecdotes.length))
+  }
+
+  const addVote = () =>{
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+    if (mostPopular == -1 || copy[selected]>copy[mostPopular])
+      setMostPopular(selected)
   }
 
 
@@ -31,14 +63,21 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
   ]
    
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
   const [selected, setSelected] = useState(0)
+  const [mostPopular, setMostPopular] = useState(-1)
 
   return (
     <div>
       {anecdotes[selected]}
       <br/>
+      Current Vote total: {votes[selected]}
       <br/>
       <Button text ="Generate Anecdote" handleClick={generateAnecdote}/>
+      <Button text ="Vote" handleClick={addVote}/>
+      <br/>
+      <Popanec popularAnecdote={mostPopular} anecdotes = {anecdotes} votes = {votes}/>
+
     </div>
   )
 }
